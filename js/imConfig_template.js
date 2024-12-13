80d3bced357e2bdfcc0d0afa0ac45414616c057a
Mikko Karvonen <mikko@immersal.com>
2024-12-13 23:55:15 +0200

    Initial commit, with Three.js support

---
+++ js/imConfig_template.js
@@ -0,0 +1,19 @@
+export const immersalParams = {
+    developerToken: "xyz", // INSERT YOUR DEVELOPER TOKEN HERE
+    mapIds: [123],  // INSERT YOUR MAP ID/IDS HERE
+    continuousLocalization: true,
+    continuousInterval: 16, // ms
+    imageDownScale: 0.25,
+    solverType: 1,
+    /* DEBUG with fixed focal length
+    deviceData: {
+      model: "Apple MacBook Pro",
+      width: 960,
+      height: 720,
+      fx: 980,
+      fy: 980,
+      ox: 480,
+      oy: 360
+    }
+    */
+  };
