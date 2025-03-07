de07ea2e10618713ca907ccca220562566da8951
John Doe <johndoe@email.com>
2025-03-07 09:13:43 -0300

    j

---
+++ js/imConfig.js
@@ -0,0 +1,19 @@
+export const immersalParams = {
+    developerToken: "709e4b5bd05224dafbedb889875a254520d1bedb155496d10740422555850642", // INSERT YOUR DEVELOPER TOKEN HERE
+    mapIds: [120205],  // INSERT YOUR MAP ID/IDS HERE
+    continuousLocalization: true,
+    continuousInterval: 16, // ms
+    imageDownScale: 0.25,
+    solverType: 1,
+    /* DEBUG with fixed focal length
+    deviceData: {
+      model: "Apple MacBook Pro",
+      width: 960,
+      height: 720,
+      fx: 980,
+      fy: 980,
+      ox: 480,
+      oy: 360
+    }
+    */
+  };
