3ad383ea2e94522ebf52a9caf8849d09367512f1
Manuel <mpalenque@gmail.com>
2025-03-20 13:20:21 -0300

    asd

--- js/imConfig.js
+++ js/imConfig.js
@@ -1,6 +1,6 @@
 export const immersalParams = {
     developerToken: "099e558e3e93cc97c4a3b2d71f09fc5113cca8567e0d02f8a61ff102ee16a815", // INSERT YOUR DEVELOPER TOKEN HERE
-    mapIds: [121210],  // INSERT YOUR MAP ID/IDS HERE 120793
+    mapIds: [120793],  // INSERT YOUR MAP ID/IDS HERE 120793
     continuousLocalization: true,
     continuousInterval: 16, // ms
     imageDownScale: 1,
